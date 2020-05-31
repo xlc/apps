f325a7385ccb6149d023eaedbd729a8ab7089bfb
Bryan Chen <xlchen1291@gmail.com>
2020-05-31 20:33:12 +1200

    Able to show kitty count

--- packages/page-kitty/src/KittyViewer.tsx
+++ packages/page-kitty/src/KittyViewer.tsx
@@ -1,8 +1,12 @@
 // Copyright 2017-2020 @polkadot/app-js authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable camelcase */
+
 import React from 'react';
 import styled from 'styled-components';
+import BN from 'bn.js';
+import { withCalls } from '@polkadot/react-api/hoc';
 
 import { Kitty } from './types';
 import KittyAvatar from './KittyAvatar';
@@ -11,19 +15,26 @@ const Wrapper = styled.section``;
 
 interface Props {
   kitties: Array<Kitty>;
+  kittiesCount: BN
 }
 
-const KittyViewer: React.FC<Props> = ({ kitties }: Props) => (
+const KittyViewer: React.FC<Props> = ({ kitties, kittiesCount: count }: Props) => (
   <Wrapper>
     <h1>Substrate Kitties</h1>
-    { kitties.length === 0 && 'No kitties'}
+    {/* { kitties.length === 0 && 'No kitties'}
     { kitties.map((k) => (
       <KittyAvatar
         dna={k.toU8a()}
         key={k.toHex()}
       />
-    )) }
+    )) } */}
+    kitty count: {count && count.toString()}
   </Wrapper>
 );
 
-export default KittyViewer;
+export default withCalls<Props>(
+  ['query.nft.nextTokenId', {
+    params: [0],
+    propName: 'kittiesCount'
+  }]
+)(KittyViewer);
--- packages/page-kitty/src/index.tsx
+++ packages/page-kitty/src/index.tsx
@@ -4,7 +4,6 @@
 import React, { useState } from 'react';
 import { AppProps as Props } from '@polkadot/react-components/types';
 import { registry } from '@polkadot/react-api';
-import { useApi } from '@polkadot/react-hooks';
 import KittyViewer from './KittyViewer';
 
 import AccountSelector from './AccountSelector';
@@ -27,13 +26,10 @@ registry.register({
 function TemplateApp ({ className }: Props): React.ReactElement<Props> {
   const [accountId, setAccountId] = useState<string | null>(null);
 
-  const { api } = useApi();
-  const kitties = [api.registry.createType('Kitty' as any, [1]), api.registry.createType('Kitty' as any, [2]), api.registry.createType('Kitty' as any, [3])];
-
   return (
     <main className={className}>
       <AccountSelector onChange={setAccountId} />
-      <KittyViewer kitties={kitties} />
+      <KittyViewer />
     </main>
   );
 }
