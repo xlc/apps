substrate-course/kitties-ui

Able to view kitties

packages/page-kitty/src/KittyViewer.tsx
@@ -9,28 +9,37 @@ import styled from 'styled-components';
 import BN from 'bn.js';
 import { withCalls } from '@polkadot/react-api/hoc';
 
-import { Kitty } from './types';
-import KittyAvatar from './KittyAvatar';
+import LoadKittyAvatar from './LoadKittyAvatar';
 
 const Wrapper = styled.section``;
 
 type Props = {
-  kitties_kittiesCount: BN
+  kitties_kittiesCount?: BN
 };
 
-const KittyViewer: React.FC<Props> = ({ kitties_kittiesCount: count }: Props) => (
-  <Wrapper>
-    <h1>Substrate Kitties</h1>
-    {/* { kitties.length === 0 && 'No kitties'}
-    { kitties.map((k) => (
-      <KittyAvatar
-        dna={k.toU8a()}
-        key={k.toHex()}
+const KittyViewer: React.FC<Props> = ({ kitties_kittiesCount }: Props) => {
+  const count = kitties_kittiesCount ? kitties_kittiesCount.toNumber() : 0;
+  const kitties = [];
+
+  for (let i = 0; i < count; ++i) {
+    kitties.push(
+      <LoadKittyAvatar
+        key={i}
+        kittyId={new BN(i)}
       />
-    )) } */}
-    kitty count: {count && count.toString()}
-  </Wrapper>
-);
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h1>Substrate Kitties</h1>
+      <h2>
+          Total kitties count: {count}
+      </h2>
+      { kitties }
+    </Wrapper>
+  );
+};
 
 export default withCalls<Props>(
   'query.kitties.kittiesCount'

xlc/apps

packages/page-kitty/src/KittyViewer.tsx
@@ -8,29 +8,37 @@ import styled from 'styled-components';
 import BN from 'bn.js';
 import { withCalls } from '@polkadot/react-api/hoc';
 
-import { Kitty } from './types';
-import KittyAvatar from './KittyAvatar';
+import LoadKittyAvatar from './LoadKittyAvatar';
 
 const Wrapper = styled.section``;
 
 interface Props {
-  kitties: Array<Kitty>;
-  kittiesCount: BN
+  kittiesCount: BN;
 }
 
-const KittyViewer: React.FC<Props> = ({ kitties, kittiesCount: count }: Props) => (
-  <Wrapper>
-    <h1>Substrate Kitties</h1>
-    {/* { kitties.length === 0 && 'No kitties'}
-    { kitties.map((k) => (
-      <KittyAvatar
-        dna={k.toU8a()}
-        key={k.toHex()}
+const KittyViewer: React.FC<Props> = ({ kittiesCount }: Props) => {
+  const count = kittiesCount ? kittiesCount.toNumber() : 0;
+  const kitties = [];
+
+  for (let i = 0; i < count; ++i) {
+    kitties.push(
+      <LoadKittyAvatar
+        key={i}
+        kittyId={new BN(i)}
       />
-    )) } */}
-    kitty count: {count && count.toString()}
-  </Wrapper>
-);
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h1>Substrate Kitties</h1>
+      <h2>
+          Total kitties count: {count}
+      </h2>
+      { kitties }
+    </Wrapper>
+  );
+};
 
 export default withCalls<Props>(
   ['query.nft.nextTokenId', {